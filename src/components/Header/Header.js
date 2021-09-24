18e70a59dc25090b358ec1d1ef7745deffc92f34
Md Mahtab Uddin <mahtab.sentu@outlook.com>
2021-09-24 21:29:17 +0600

    nav decoration

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import logo from '../../images/logo.png'
+import './Header.css'
 
 const header = () => {
     return (
-        <div>
-            <h1>this is header</h1>
-            <img src={logo} alt="" />
+        <div className="header">
+            
+            <img className="logo" src={logo} alt="" />
+            <nav>
+                <a href="/shop">Shops</a>
+                <a href="/order">Ordeers</a>
+                <a href="/contact">contact Us</a>
+                </nav>
         </div>
     );
 };
