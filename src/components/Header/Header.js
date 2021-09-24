d0cd52213a3b83c5ccec4045b77c127ef29e0959
Md Mahtab Uddin <mahtab.sentu@outlook.com>
2021-09-24 20:47:45 +0600

    create component and add logo

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from '../../images/logo.png'
+
+const header = () => {
+    return (
+        <div>
+            <h1>this is header</h1>
+            <img src={logo} alt="" />
+        </div>
+    );
+};
+
+export default header;
